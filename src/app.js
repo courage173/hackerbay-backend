ad45a954d3aa97bca7abc9c1c6f8f18d7a36e87f
courage173 <courageosemwengie@gmail.com>
2020-04-23 23:56:28 +0100

    created express server and babelrc file

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+export default app;
